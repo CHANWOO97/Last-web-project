e3371a849985a7a61d1ddb11c886f8c5631a5803
User-2024VFCXJC\Administrator <rkaehd127@gmail.com>
2025-03-31 15:24:37 +0900

    git commit -m "Initial Spring Boot 프로젝트 업로드"

---
+++ src/main/java/com/green/Lupang/LupangApplication.java
@@ -0,0 +1,13 @@
+package com.green.Lupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LupangApplication.class, args);
+	}
+
+}
