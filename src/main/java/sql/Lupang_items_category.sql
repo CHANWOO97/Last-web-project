4cfa59c5d7ff654bc6f56e78c0be09535cdaabfe
User-2024VFCXJC\Administrator <rkaehd127@gmail.com>
2025-04-02 14:49:04 +0900

    상품 샘플 데이터 sql 추가 필요

--- src/main/java/sql/Lupang_items_category.sql
+++ src/main/java/sql/Lupang_items_category.sql
@@ -12,5 +12,21 @@ INSERT INTO items_category (ic_id, ic_name) VALUES
 ('cat005', '미디어/오피스'),
 ('cat006', '완구/반려동물');
 
+-- 상품 데이터 샘플 추가 10개
+CREATE TABLE items (
+   	i_id VARCHAR(255) NOT NULL COMMENT '상품코드',
+    ic_id VARCHAR(255) NOT NULL COMMENT '카테고리 ID (FK: items_category.ic_id)',
+    name VARCHAR(255),
+    size VARCHAR(10) NOT NULL DEFAULT 'M' COMMENT '사이즈 (S/M/L)',
+    photo VARCHAR(255),
+    location VARCHAR(255) NOT NULL,
+    stock INT NOT NULL,
+    price INT NOT NULL,
+    reg_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    description TEXT,
+    PRIMARY KEY (i_id)
+);
+
+
 SELECT * FROM items_category;
 delete from items_category;
