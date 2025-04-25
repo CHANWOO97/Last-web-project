f7d0c10ac5657c9d27388042645f2a95e75fab5d
User-2024VFCXJC\Administrator <rkaehd127@gmail.com>
2025-04-25 18:09:54 +0900

    salecontroller.java order.jsp 매핑 추가

--- src/main/java/com/green/Lupang/controller/SaleController.java
+++ src/main/java/com/green/Lupang/controller/SaleController.java
@@ -38,14 +38,41 @@ public class SaleController {
 	@Autowired
 	private IamportService ims;
 	
+	@PostMapping("/itemsOrder/orderDirect")
+	public String orderDirect(@RequestParam("selectedItems") List<String> selectedItems, Model model, HttpSession session) {
+		String id = (String)session.getAttribute("id");
+		User user = us.select(id);
+		List<SaleItems> saleItems = new ArrayList<>();	
+		List<ItemsCategory> ic_list = is.ic_list();		
+		for (String selectItem : selectedItems) { // selectedItems 값을 ${item.i_id}_${item.quantity} 이렇게 받아옴
+			String [] parts = selectItem.split("_");	
+			String i_id = parts[0];
+			Items item = is.select(i_id);
+			int quantity = Integer.parseInt(parts[1]);
+			SaleItems si = new SaleItems();
+			si.setName(item.getName()); si.setPhoto(item.getPhoto());
+			si.setPrice(item.getPrice()); si.setQuantity(quantity);
+			si.setI_id(item.getI_id());
+			saleItems.add(si);		
+		}
+		model.addAttribute("user", user);
+		model.addAttribute("ic_list", ic_list);
+		model.addAttribute("saleItems", saleItems);
+		return "/itemsOrder/order"; // order.jsp로 이동
+	}
+	
 	@PostMapping("/itemsOrder/order")
 	public String itemsOrder(@RequestParam("selectedItems") List<String> selectedItems, Model model, HttpSession session) {
 		String id = (String)session.getAttribute("id");
 		User user = us.select(id);
 		List<SaleItems> saleItems = new ArrayList<>();	
 		List<ItemsCategory> ic_list = is.ic_list();		
-		for (String i_id : selectedItems) { // selectedItems 값을 ${item.i_id}_${item.quantity} 이렇게 받아옴
+		for (String selectItem : selectedItems) { // selectedItems 값을 ${item.i_id}_${item.quantity} 이렇게 받아옴
+			String [] parts = selectItem.split("_");	
+			String i_id = parts[0];
 			Items item = is.select(i_id);
+			System.out.println(id);
+			System.out.println(i_id);
 			int quantity = cs.findCartByUserAndItem(id, i_id);
 			SaleItems si = new SaleItems();
 			si.setName(item.getName()); si.setPhoto(item.getPhoto());
